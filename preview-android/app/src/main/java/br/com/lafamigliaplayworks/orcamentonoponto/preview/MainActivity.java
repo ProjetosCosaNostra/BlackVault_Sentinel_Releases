dd20b57971021eeecbc317b05a4dc009dd878290
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-21 15:37:28 -0300

    preview: use modern WindowInsets fullscreen APIs

--- preview-android/app/src/main/java/br/com/lafamigliaplayworks/orcamentonoponto/preview/MainActivity.java
+++ preview-android/app/src/main/java/br/com/lafamigliaplayworks/orcamentonoponto/preview/MainActivity.java
@@ -12,6 +12,10 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -19,14 +23,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(Color.rgb(2,3,3));
         getWindow().setNavigationBarColor(Color.rgb(2,3,3));
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        );
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat controller =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.hide(WindowInsetsCompat.Type.systemBars());
+        controller.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
         setContentView(new PreviewView());
     }
 
