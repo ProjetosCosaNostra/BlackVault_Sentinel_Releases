066bad5bbd7e0bbdded3fe3fe065b8bb7cd73472
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-21 14:49:06 -0300

    preview: validate ecosystem in immersive runtime

--- preview-android/app/src/main/java/br/com/lafamigliaplayworks/orcamentonoponto/preview/MainActivity.java
+++ preview-android/app/src/main/java/br/com/lafamigliaplayworks/orcamentonoponto/preview/MainActivity.java
@@ -19,6 +19,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setStatusBarColor(Color.rgb(2,3,3));
         getWindow().setNavigationBarColor(Color.rgb(2,3,3));
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
         setContentView(new PreviewView());
     }
 
