56413e5174924b5d2846e582a7f8083700fdc43b
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-20 23:12:43 -0300

    ONP cloud preview: app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+  namespace = "br.com.lafamigliaplayworks.onpcloudpreview"
+  compileSdk = 35
+  defaultConfig {
+    applicationId = "br.com.lafamigliaplayworks.onpcloudpreview"
+    minSdk = 26
+    targetSdk = 35
+    versionCode = 37
+    versionName = "37-cloud-preview"
+  }
+}
