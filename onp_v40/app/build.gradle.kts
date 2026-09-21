98f1202b9a0f5ccf2a9cc93a36dbc13cbd4383f1
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-21 00:18:08 -0300

    ONP V40 build: add onp_v40/app/build.gradle.kts

---
+++ onp_v40/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+  namespace = "br.com.lafamigliaplayworks.orcamentonoponto.recovery"
+  compileSdk = 35
+
+  defaultConfig {
+    applicationId = "br.com.lafamigliaplayworks.orcamentonoponto.recovery"
+    minSdk = 26
+    targetSdk = 35
+    versionCode = 40
+    versionName = "40-visual-recovery"
+  }
+}
